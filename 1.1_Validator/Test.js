17b9f86f2aa29bcdf0e9e4f011f88bcf1d4f18dc
Alex <139467897+Alex-LaNN@users.noreply.github.com>
2023-08-11 11:40:16 +0300

    №10 Validator completed

--- 1.1_Validator/Test.js
+++ 1.1_Validator/Test.js
@@ -1,53 +1,55 @@
 const Validator = require("./Validator");
 
-// Класс тестирования.
+// Test function.
 function runTests() {
-  // Вод информации первым пользователем.
+  // Information entered by the first user.
   const user0 = new Validator(
     "fi@seconDpart.end",
     "+38 (099) 567 8901",
     "C00l_Pass"
   );
 
-  // Вод информации вторым пользователем.
+  // Information entered by the second user.
   const user1 = new Validator(
     "first-part@.se=cond%p.art.end",
     "+12 099 5 6 7-8 9  01",
-    "SupperPas1"
+    //    "SupperPas1"
+    "upper_as1"
   );
 
-  // Вод информации третьим пользователем.
+  // Information entered by a third user.
   const user2 = new Validator(
     "firSt.part@se=cond%part.r",
     "(09-9) 567-890-1",
-    "Next_SuperPass0"
+    //    "Next_SuperPass0"
+    "NEXT_SUPERPASSO0"
   );
 
-  // Вод информации четвертым пользователем.
+  // Information entered by the fourth user.
   const user3 = new Validator("1+@’.Y", "--  (099) 567 890-1", "BigPassword0");
 
-  // Вод информации пятым пользователем.
+  // Information entered by the fifth user.
   const user4 = new Validator(
     "f@secondart.end",
     "+38 (099) 567 8901 0",
     "Cool_pass"
   );
 
-  // Вод информации шестым пользователем.
+  // Information entered by the sixth user.
   const user5 = new Validator(
     "first-part@.se=cond@part.end",
     "+38 099 a0000012",
     "C00l"
   );
 
-  // Вод информации седьмым пользователем.
+  // Information entered by the seventh user.
   const user6 = new Validator(
     "-firstpart@.se=cOnd%.enddeded",
     "+38 (0989) 567 8901",
     "_passNext"
   );
 
-  // Вод информации восьмым пользователем.
+  // Information entered by the eighth user.
   const user7 = new Validator(
     "firs_tpart@.se.en",
     "+48 (0989) 567 8901",
@@ -57,7 +59,7 @@ function runTests() {
   // Create a new instance of the 'Validator' class.
   const validator = new Validator();
 
-  // Создание базы данных.
+  // Creation of a shared database.
   validator.addData(user0);
   validator.addData(user1);
   validator.addData(user2);
@@ -67,23 +69,23 @@ function runTests() {
   validator.addData(user6);
   validator.addData(user7);
 
-  // Обработка базы данных.
+  // Database processing.
   for (let i = 0; i < validator.dataLists.length; i++) {
-    // Получение всех данных для проверки, введенных определенным пользователем.
+    // Receiving all data entered by a specific user for verification.
     const dataListValue = validator.dataLists[i];
-    // Результат проверки корректности введенного пользователем 'Email'.
+    // The result of checking the correctness of the form of the 'Email' entered by the user.
     const resultValidateEmail = validator.validateEmail(
       dataListValue.getEmail()
     );
-    // Результат проверки корректности введенного пользователем 'Password'.
+    // The result of checking the correctness of the form entered by the user 'Password'.
     const resultValidatePassword = validator.validatePassword(
       dataListValue.getPassword()
     );
-    // Результат проверки корректности введенного пользователем 'Phone'.
+    // The result of checking the correctness of the view entered by the user 'Phone'.
     const resultValidatePhone = validator.validatePhone(
       dataListValue.getPhone()
     );
-    // Генерация и вывод на консоль всей информации, получаемой при тестировании.
+    // Generation and output to the console of all information obtained during testing.
     getResultForConsole(
       resultValidateEmail,
       i,
@@ -93,7 +95,7 @@ function runTests() {
     );
   }
 
-  // Генерация и вывод на консоль всей информации, получаемой при тестировании.
+  // Generation and output to the console of all information obtained during testing.
   function getResultForConsole(
     resultValidateEmail,
     i,
@@ -115,7 +117,7 @@ function runTests() {
       );
     } else
       console.log(
-        `User${i} Password введен корректно : ${dataListValue.getPassword()}`
+        `User${i} Password введен корректно   : ${dataListValue.getPassword()}`
       );
     if (!resultValidatePhone) {
       console.log(
--- 1.1_Validator/Validator.js
+++ 1.1_Validator/Validator.js
@@ -1,4 +1,4 @@
-// Коллекция методов для валидации полей ввода данных и получение различной информации о некорректно введенных данных.
+// A collection of methods for validating data entry fields and getting various information about the results of this validation.
 class Validator {
   constructor(email, phone, password) {
     this.email = email;
@@ -10,27 +10,27 @@ class Validator {
     this.results = [];
   }
 
-  //
+  // Returns the Email.
   getEmail() {
     return this.email;
   }
 
-  //
+  // Returns the Phone.
   getPhone() {
     return this.phone;
   }
 
-  //
+  // Returns the Password.
   getPassword() {
     return this.password;
   }
 
-  //
+  // A record of all data fields entered by the user.
   addData(dataList) {
     return this.dataLists.push(dataList);
   }
 
-  //
+  // Record according to the unique user identifier of the received validation results of all user-entered data fields.
   addResult(
     userId,
     resultValidateEmail,
@@ -45,41 +45,37 @@ class Validator {
     );
   }
 
-  //
-  addResults(ValidateEnteredParametersOfUser) {
-    return this.results.push(ValidateEnteredParametersOfUser);
-  }
-
-  //
+  // Check for the correctness of the string with the entered 'email'.
   validateEmail(email) {
-    // Проверка на корректность вводимого email-адреса.
+    // Regular expression used for character-by-character validation of the string being checked.
     const regexp =
       /^[a-z0-9][a-z0-9.+-]{1,19}@[a-z0-9.!$%&*+/=?^_-]{1,15}\.[a-z]{1,3}$/iu;
     return regexp.test(email);
   }
 
-  // Проверка на корректность вводимого номера телефона.
+  // Check for the correctness of the string with the entered phone number.
   validatePhone(phone) {
-    // Проверка максимального количества введенных символов в строке (телефоне).
+    // Check the maximum number of entered characters in a string 'phone'.
     if (phone.length > 25) {
       return false;
     } else {
-      // Выбрать из строки только цифры.
+      // Select only numbers from a string.
       const cleanedPhone = phone.replace(/[^\d]/g, "");
       const numberSize = cleanedPhone.length;
-      // Количественно-численная проверка корректности введенного пользователем номера телефона.
+      // Quantitative-numerical preliminary check of the correctness of the phone number entered by the user.
       if (numberSize !== 12 && numberSize !== 10) return false;
     }
-    // Посимвольная проверка корректности номера телефона.
+    // Regular expression used for character-by-character validation of the string being checked.
     const regexp = /^(\+\d\d)?[\s-]*\(?\d{3}[\s-]*\)?([\s-]*\d){7}$/u;
     return regexp.test(phone);
   }
 
-  // Проверка на корректность устанавливаемого значения пароля.
-  validatePassword() {}
-
-  // Вывод имени пользователя 'userId' и некорректно введенные им поля данных.
-  getIncorrectlyEnteredData() {}
+  // Checking for correctness of the string containing the 'password' value.
+  validatePassword(password) {
+    // Regular expression used for character-by-character validation of the string being checked.
+    const regexp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d_]{8,}$/;
+    return regexp.test(password);
+  }
 }
 
 module.exports = Validator;
